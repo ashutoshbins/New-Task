320e5ece171d9b90ccea818b30c82f4be33eb613
Ashutosh <113531606+ashutoshbins@users.noreply.github.com>
2025-05-07 08:13:47 +0530

    Update historical_data.py

--- modules/historical_data.py
+++ modules/historical_data.py
@@ -2,7 +2,8 @@ import requests
 import pandas as pd
 
 def fetch_historical_data(symbol, start_date):
-    api_url = "https://historicalapi-js.onrender.com/historical"
+    #api_url = "https://historicalapi-js.onrender.com/historical"
+    api_url = "https://4d23edcc-c01a-4fa1-843c-d10f5859c722-00-13kh8zyk0cgze.riker.replit.dev/historicalapi/historical"
     params = {"symbol": symbol, "from": start_date.strftime("%d-%m-%Y")}
     response = requests.get(api_url, params=params)
     if response.status_code == 200:
